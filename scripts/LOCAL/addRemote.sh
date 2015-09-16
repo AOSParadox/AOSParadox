03279132efbc5567161d8ba877bf1568af6ce1ae
Louis Popi <theh2o64@gmail.com>
2015-09-16 19:16:51 +0200

    scripts : Add MSM8960 support and general cleanup 	* Add a script to add alll remotes 	* lollipop_common now use the latestest apps possible

---
+++ scripts/LOCAL/addRemote.sh
@@ -0,0 +1,147 @@
+#/bin/sh
+
+cd build/
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_build.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd device/qcom/common
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_device_qcom_common.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd device/qcom/msm8974
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_device_qcom_msm8974.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd device/qcom/msm8226
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_device_qcom_msm8226.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd device/qcom/msm8960
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_device_qcom_msm8226.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd device/qcom/sepolicy
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_device_qcom_sepolicy.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd frameworks/base
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_frameworks_base.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd hardware/qcom/audio
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_hardware_qcom_audio.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd hardware/qcom/bt
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_hardware_qcom_bt.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd hardware/qcom/keymaster
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_hardware_qcom_keymaster.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd hardware/qcom/wlan
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_hardware_qcom_wlan.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd hardware/ril
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_hardware_ril.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd kernel/msm
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/kernel_msm.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/SoundRecorder
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_SoundRecorder.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Dialer
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Dialer.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Mms
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Mms.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/MusicFX
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_MusicFX.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Settings
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Settings.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/SnapdragonCamera
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_SnapdragonCamera.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/LegacyCamera
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_LegacyCamera.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Music
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Music.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Gallery2
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Gallery2.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Gallery
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Gallery.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Camera
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Camera.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Browser
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Browser.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd packages/apps/Camera2
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_packages_apps_Camera2.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd system/core
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_system_core.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd system/qcom
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_system_qcom.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
+cd vendor/qcom/opensource/bluetooth
+git remote remove aosparadox
+git remote add aosparadox git@github.com:AOSParadox/android_vendor_qcom_opensource_bluetooth.git
+cd /home/louis/Dev/AOSPARADOX_FALCON/
+
--- scripts/NAS/cafUpdateTool.sh
+++ scripts/NAS/cafUpdateTool.sh
@@ -2,6 +2,7 @@
 
 revision_msm8226=LA.BF.1.1.1.c5
 revision_msm8974=LA.BF.1.1.1.c4
+revision_newest=LA.BR.1.2.5_rb2.14
 
 function extract_8226() {
     for FILE in `egrep -v '(^#|^$)' $1`; do
@@ -35,5 +36,40 @@ function extract_8974() {
     done
 }
 
-extract_8226 msm8226
-extract_8974 msm8974
+function extract_8960() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+	cd $FILE
+	git fetch caf $revision_msm8960
+	git checkout -b $revision_msm8960 caf/$revision_msm8960
+	git push aosparadox $revision_msm8960
+	cd /home/nas/EXTRA/CAF/
+    done
+}
+
+function extract_common() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+        OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+        FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+        DEST=${PARSING_ARRAY[1]}
+        if [ -z $DEST ]; then
+            DEST=$FILE
+        fi
+	cd $FILE
+	git fetch caf $revision_newest
+	git checkout -b $revision_newest caf/$revision_newest
+	git push aosparadox $revision_newest
+	cd /home/nas/EXTRA/CAF/
+    done
+}
+
+extract_8226 lollipop_msm8226
+extract_8974 lollipop_msm8974
+extract_8960 lollipop_msm8960
+extract_8974 lollipop_bacon
+extract_common lollipop_common
